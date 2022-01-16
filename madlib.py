5ec0c93e7171341479f88745560d9f8044f7ade9
CoderPOOP <95227359+CoderPOOP@users.noreply.github.com>
2022-01-16 14:27:19 +0530

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,10 @@
+# Creating a Simple MadLib
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb 2: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer Programming is so {adj}! It makes me so excited because I love to {verb1}. Stay Hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
